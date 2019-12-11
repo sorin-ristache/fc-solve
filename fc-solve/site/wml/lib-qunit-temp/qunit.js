79a4d418e2b3c79152525daa19a2c3d95d64dfba
Shlomi Fish <shlomif@shlomifish.org>
2019-12-11 17:34:38 +0200

    debug emcc; reduce code

--- fc-solve/site/wml/lib-qunit-temp/qunit.js
+++ fc-solve/site/wml/lib-qunit-temp/qunit.js
@@ -3138,21 +3138,6 @@
   		return button;
   	}
 
-  	function moduleListHtml() {
-  		var i,
-  		    checked,
-  		    html = "";
-
-  		for (i = 0; i < config.modules.length; i++) {
-  			if (config.modules[i].name !== "") {
-  				checked = config.moduleId.indexOf(config.modules[i].moduleId) > -1;
-  				html += "<li><label class='clickable" + (checked ? " checked" : "") + "'><input type='checkbox' " + "value='" + config.modules[i].moduleId + "'" + (checked ? " checked='checked'" : "") + " />" + escapeText(config.modules[i].name) + "</label></li>";
-  			}
-  		}
-
-  		return html;
-  	}
-
   	// HTML Reporter initialization and load
   	QUnit.begin(function (details) {
   		var i, moduleObj;
