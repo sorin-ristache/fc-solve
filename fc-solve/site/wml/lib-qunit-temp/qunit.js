1e37d0174ff6eb91d960e333cd5f2fe7c5998ba2
Shlomi Fish <shlomif@shlomifish.org>
2019-12-11 16:51:09 +0200

    debug emcc

--- fc-solve/site/wml/lib-qunit-temp/qunit.js
+++ fc-solve/site/wml/lib-qunit-temp/qunit.js
@@ -127,24 +127,6 @@
   	setTimeout: setTimeout$1 !== undefined
   };
 
-  // Returns a new Array with the elements that are in a but not in b
-  function diff(a, b) {
-  	var i,
-  	    j,
-  	    result = a.slice();
-
-  	for (i = 0; i < result.length; i++) {
-  		for (j = 0; j < b.length; j++) {
-  			if (result[i] === b[j]) {
-  				result.splice(i, 1);
-  				i--;
-  				break;
-  			}
-  		}
-  	}
-  	return result;
-  }
-
   /**
    * Determines whether an element exists in a given array or not.
    *
